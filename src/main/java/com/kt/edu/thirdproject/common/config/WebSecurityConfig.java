20205d9c40a1ddaff95f060b2cc8e44616e8e779
heewon00 <55778040+heewon00@users.noreply.github.com>
2024-02-20 22:51:19 +0900

    240220

--- src/main/java/com/kt/edu/thirdproject/common/config/WebSecurityConfig.java
+++ src/main/java/com/kt/edu/thirdproject/common/config/WebSecurityConfig.java
@@ -44,12 +44,16 @@ public class WebSecurityConfig {
                 .formLogin(Customizer.withDefaults());
          http
                 .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
-                        //.requestMatchers(HttpMethod.GET, "/api/v1/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "api/v1/employees", "api/vi/employees/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/actuator/**").permitAll()
-                        //.requestMatchers("/api/v1/employees").hasAnyRole(EDU_MANAGER, EDU_USER)
-                        .requestMatchers(HttpMethod.POST,"/api/v1/employees").hasAnyRole("EDU_MANAGER")
-                        .requestMatchers(HttpMethod.GET,"/api/v1").hasAnyRole("EDU_MANAGER", "EDU_USER")
+                        .requestMatchers("api/v1/employees", "api/v1/employees/**","api/v1/employee/**").hasRole(EDU_MANAGER)
                         .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs", "/v3/api-docs/**").permitAll()
+                        //.requestMatchers(HttpMethod.GET, "/api/v1/**").permitAll()
+                        //.requestMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+                        //.requestMatchers("/api/v1/employees").hasAnyRole(EDU_MANAGER, EDU_USER)
+                        //.requestMatchers(HttpMethod.POST,"/api/v1/employees").hasAnyRole("EDU_MANAGER")
+                        //.requestMatchers(HttpMethod.GET,"/api/v1").hasAnyRole("EDU_MANAGER", "EDU_USER")
+                        //.requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(
                         jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter)))
