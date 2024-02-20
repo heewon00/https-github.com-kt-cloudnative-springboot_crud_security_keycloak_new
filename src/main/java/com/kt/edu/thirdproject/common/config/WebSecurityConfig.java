d3b17efceab109105ea41dc4c44f537760e77af0
heewon00 <55778040+heewon00@users.noreply.github.com>
2024-02-20 22:26:06 +0900

    240220

--- src/main/java/com/kt/edu/thirdproject/common/config/WebSecurityConfig.java
+++ src/main/java/com/kt/edu/thirdproject/common/config/WebSecurityConfig.java
@@ -47,8 +47,8 @@ public class WebSecurityConfig {
                         //.requestMatchers(HttpMethod.GET, "/api/v1/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/actuator/**").permitAll()
                         //.requestMatchers("/api/v1/employees").hasAnyRole(EDU_MANAGER, EDU_USER)
-                        .requestMatchers(HttpMethod.GET,"/api/v1/**").hasAnyRole("EDU_MANAGER", "EDU_USER")
-                        .requestMatchers("/api/v1/employees","api/v1/employee/**","api/v1/employees/**").hasAnyRole("EDU_MANAGER")
+                        .requestMatchers(HttpMethod.POST,"/api/v1/employees","api/v1/employee/**","api/v1/employees/**").hasAnyRole("EDU_MANAGER")
+                        .requestMatchers(HttpMethod.GET,"/api/v1").hasAnyRole("EDU_MANAGER", "EDU_USER")
                         .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(
